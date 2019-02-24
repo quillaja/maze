0e4cc7eb2ca85ea08a08b64118702c65fa6e2ae1
Ben <quillaja@hotmail.com>
2019-02-23 21:10:58 -0800

    rethought some of the interfaces, in particular Graph was added.

--- types.go
+++ types.go
@@ -1,13 +1,13 @@
 package maze
 
-import (
-	"sort"
-)
+// TODO: for future consideration.
+// type void struct{}
+// type nodeSet map[Node]void
 
 // Node allows types to become "nodes" or "cells" in a maze, which is
 // essentially just an undirected graph.
 type Node interface {
-	Neighbors() NodeSlice
+	IsNeighbor(other Node) bool
 }
 
 // NodeSlice is a slice of Nodes.
@@ -24,15 +24,15 @@ func ToNodeSlice(list []interface{}) NodeSlice {
 }
 
 // Append adds the nodes to the slice.
-func (slice NodeSlice) append(n ...Node) NodeSlice {
+func (slice NodeSlice) Append(n ...Node) NodeSlice {
 	return append(slice, n...)
 }
 
 // AppendUnique adds the node to the slice only if it is not already
 // in the slice.
-func (slice NodeSlice) appendUnique(n Node) NodeSlice {
-	if !slice.has(n) {
-		slice = slice.append(n)
+func (slice NodeSlice) AppendUnique(n Node) NodeSlice {
+	if !slice.Has(n) {
+		slice = slice.Append(n)
 	}
 	return slice
 }
@@ -40,13 +40,20 @@ func (slice NodeSlice) appendUnique(n Node) NodeSlice {
 // Index returns the first index where 'n' is found, or 'len(slice)'
 // if the node is not found.
 func (slice NodeSlice) index(n Node) int {
-	return sort.Search(len(slice), func(j int) bool {
-		return slice[j] == n
-	})
+	// sort.Search() doesn't work as a general purpose search.
+	// return sort.Search(len(slice), func(j int) bool {
+	// 	return slice[j] == n
+	// })
+	for i := 0; i < len(slice); i++ {
+		if slice[i] == n {
+			return i
+		}
+	}
+	return len(slice)
 }
 
 // Has returns true if the slice contains 'n'.
-func (slice NodeSlice) has(n Node) bool {
+func (slice NodeSlice) Has(n Node) bool {
 	return slice.index(n) != len(slice)
 }
 
@@ -67,7 +74,7 @@ func (slice NodeSlice) removeAt(i int) NodeSlice {
 
 // Remove removes the first occurence of 'n' from the slice. The
 // original order is not preserved.
-func (slice NodeSlice) remove(n Node) NodeSlice {
+func (slice NodeSlice) Remove(n Node) NodeSlice {
 	i := slice.index(n)
 	if i < len(slice) {
 		return slice.removeAt(i)
@@ -82,57 +89,98 @@ func (slice NodeSlice) pop() (NodeSlice, Node) {
 	return slice.removeAt(len(slice) - 1), n
 }
 
-type nodeSet map[Node]struct{}
+// Graph provides the basic interface needed by maze making algorithms, since
+// mazes are essentially just undirected graphs.
+type Graph interface {
+	// Has returns true if the graph contains Node.
+	Has(Node) bool
+	// Adds the node(s) to the graph.
+	Add(...Node)
+	// Removes the node(s) from the graph and any edges associated with the node(s).
+	Remove(...Node)
+	// Neighbors provides a list of nodes connected to Node.
+	// It is expected to return an empty NodeSlice if the Node has no associated
+	// edges, and nil if the Node is not in the graph.
+	Neighbors(Node) NodeSlice
+	// HasEdge returns true if a and b are connected by an edge.
+	HasEdge(a, b Node) bool
+	// AddEdge adds an undirected edge between a and b. It may add nodes a
+	// and/or b if they are not already in the graph.
+	AddEdge(a, b Node)
+	// RemoveEdge removes the edge between a and b if a, b, and the edge are
+	// in the graph.
+	RemoveEdge(a, b Node)
+}
 
-// Graph maintains an undirected graph of Nodes and edges between Nodes.
-type graph map[Node]NodeSlice
+// mapgraph maintains an undirected graph of Nodes and edges using a map.
+type mapgraph map[Node]NodeSlice
+
+// NewMapGraph makes a Graph using a map. Therefore it is suitable for graphs
+// with an arbitrary topology.
+func NewMapGraph() Graph {
+	return make(mapgraph)
+}
 
 // Has returns true if the node 'n' is in the graph.
-func (g graph) has(n Node) bool {
+func (g mapgraph) Has(n Node) bool {
 	_, in := g[n]
 	return in
 }
 
-// AddNode adds the node to the graph.
-func (g graph) addNode(nodes ...Node) {
+// Adds the node(s) to the graph.
+func (g mapgraph) Add(nodes ...Node) {
 	for _, n := range nodes {
-		if !g.has(n) {
-			g[n] = n.Neighbors() // NOTE: do addEdge() instead of this?
+		if !g.Has(n) {
+			g[n] = make(NodeSlice, 0)
 		}
 	}
 }
 
-// AddEdge adds an undirected edge connecting a and b.
-func (g graph) addEdge(a, b Node) {
-	g.addNode(a, b)
-	g[a] = g[a].appendUnique(b)
-	g[b] = g[b].appendUnique(a)
+// Removes the node(s) from the graph. It also removes edges between the
+// deleted nodes and their former neighbors.
+func (g mapgraph) Remove(nodes ...Node) {
+	for _, n := range nodes {
+		// remove edges so that current neighbors do not maintain
+		// "dangling" directed edges to n
+		for _, neighbor := range g.Neighbors(n) {
+			g.RemoveEdge(n, neighbor)
+		}
+		// remove the node
+		delete(g, n)
+	}
 }
 
-// RemoveEdge removes the edge connecting a and b.
-func (g graph) removeEdge(a, b Node) {
-	if !g.has(a) || !g.has(b) {
+// Neighbors returns the nodes with an edge to n. If n is isolated, an empty
+// NodeSlice is returned. If n is not in the graph, nil is returned.
+func (g mapgraph) Neighbors(n Node) NodeSlice {
+	return g[n]
+}
+
+// HadEdge returns true if a and b and connected with an edge. False is returned
+// if a and b are not connected or if a is not in the graph.
+func (g mapgraph) HasEdge(a, b Node) bool {
+	n, in := g[a]
+	if !in {
+		return false
+	}
+	return n.Has(b)
+}
+
+// AddEdge adds an undirected edge connecting a and b. Nodes a and b are added
+// to the graph if not already present.
+func (g mapgraph) AddEdge(a, b Node) {
+	g.Add(a, b)
+	g[a] = g[a].AppendUnique(b)
+	g[b] = g[b].AppendUnique(a)
+}
+
+// RemoveEdge removes the edge connecting a and b. It does nothing if a, b, or
+// the edge are not in the graph.
+func (g mapgraph) RemoveEdge(a, b Node) {
+	if !g.Has(a) || !g.Has(b) || !g.HasEdge(a, b) {
 		return
 	}
 
-	g[a] = g[a].remove(b)
-	g[b] = g[b].remove(a)
-}
-
-// ThreeToOne calculates the 1D index from a 3D index given the indices
-// 'x, y, z' and the x-row width 'dx' and the y-column height 'dy'. Both
-// dx and dy should be in the range [1, N].
-func ThreeToOne(x, y, z, dx, dy int) int {
-	return x + y*dx + z*dx*dy
-}
-
-// OneToThree calculates the 3D indices from a 1D index given
-// the x-row width 'dx' and the y-column height 'dy'. Both
-// dx and dy should be in the range [1, N].
-func OneToThree(i, dx, dy int) (x, y, z int) {
-	z = i / (dx * dy)
-	i = i % (dx * dy)
-	y = i / dx
-	x = i % dx
-	return
+	g[a] = g[a].Remove(b)
+	g[b] = g[b].Remove(a)
 }
